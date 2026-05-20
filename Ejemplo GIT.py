68eb22948a9c75b02c553654875700041f1bf4e6
Marianatrinidad <marianatrinidadf@gmail.com>
2026-05-19 19:55:26 -0600

    Factor 200

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -6,5 +6,9 @@ c = 30 * 10 * 5
 
 print('resultado=', c)
 
+## Cambiando el factor 102 según requerimiento de negocio
+## Cambiando el factor 110 según Junta Control
+## Cambio a factor 200 por Directorio
+
 d = c * 100
 print ("resultado total=", d)
