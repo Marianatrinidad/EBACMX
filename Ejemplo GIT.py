56aebfac82d64afbdbd62d53b7b5daccfc81cda8
Marianatrinidad <mariana.trininidadf@gmail.com>
2026-05-21 12:39:09 -0600

    Añado a variable f

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -1,10 +1,12 @@
 # Prueba de Python
 # Se añadio la variable c
 # nueva variable E
+# Añado la variable f como factor de tendencia
 a = 10
 b = 30
 e = 1.1
-c = 30 * 10 * 5 / e
+f = 5
+c = 30 * 10 * 5 / e (1 + f/100)
 print('resultado=', c)
 
 
