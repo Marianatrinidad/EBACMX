123589f38691569f42191df785b36f8349ec29b9
Marianatrinidad <marianatrinidadf@gmail.com>
2026-05-20 20:18:14 -0600

    nueva variable E

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -1,10 +1,13 @@
 # Prueba de Python
 # Se añadio la variable c
+# nueva variable E
 a = 10
 b = 30
-c = 30 * 10 * 5
-
+e = 1.1
+c = 30 * 10 * 5 / e
 print('resultado=', c)
 
-d = c * 100
+
+
+d = c * 300
 print ("resultado total=", d)
