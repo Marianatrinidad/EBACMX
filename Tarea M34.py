0c989055f8262a5bfa0ac9585a7fba8842f0015e
Marianatrinidad <marianatrinidadf@gmail.com>
2026-05-24 13:32:36 -0600

    Creación de repositorio - Tarea M34

---
+++ Tarea M34.py
@@ -0,0 +1,12 @@
+# Tarea M34
+
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import plotly.express as px
+import matplotlib as plt
+
+os.chdir('/users/maria/downloads')
+
+df = pd.read_csv('ventas_catering.csv')
